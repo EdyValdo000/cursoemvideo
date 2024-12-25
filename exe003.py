6af97d8a201df21e681e40f19bdb322bb33147f8
EdyValdo000 <edivaldocoelho000@example.com>
2024-12-25 18:33:41 +0100

    Exercícios 3 e 4 terminados

---
+++ exe003.py
@@ -0,0 +1,5 @@
+number1 = int(input('digite um valor: '))
+number2 = int(input('digite mais um valor: '))
+add = number1 + number2
+print('A soma de {} com {} é igual a {}'.format(number1,number2,add))
+# comentário é assim;
