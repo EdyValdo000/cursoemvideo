962d8c0112f9a6385048d4e4bea37b41b33e4490
EdyValdo000 <edivaldocoelho000@example.com>
2024-12-30 20:55:34 +0100

    Terminei de ajustar alguns exercícios

--- exe028.py
+++ exe028.py
@@ -1,5 +1,8 @@
 from random import randint
+from time import sleep
 number = int(input('Digite um número de 0 até 5: '))
+print('Processando...')
+sleep(2)
 sorte = randint(0,5)
 if number == sorte:
     print('Acertaste o número sorteado')
--- exe032.py
+++ exe032.py
@@ -5,14 +5,28 @@ Se não for divisível por 100, é bissexto.
 Se for divisível por 100, veja se também é divisível por 400.
 Se for, é bissexto.
 Caso contrário, não é bissexto."""
+from datetime import date
 year = int(input('Digite o ano: '))
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print('O ano é bissexto.')
+if year == 0:
+    year = date.today().year
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print('O ano {} é bissexto.'.format(year))
+            else:
+                print('O ano {} não é bissexto.'.format(year))
         else:
-            print('O ano não é bissexto.')
+            print('O ano {} é bissexto.'.format(year))
     else:
-        print('O ano é bissexto.')
+        print('O ano {} não é bissexto.'.format(year))
 else:
-    print('O ano não é bissexto.')
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print('O ano {} é bissexto.'.format(year))
+            else:
+                print('O ano {} não é bissexto.'.format(year))
+        else:
+            print('O ano {} é bissexto.'.format(year))
+    else:
+        print('O ano {} não é bissexto.'.format(year))
