955d84935ddecea76b8f004c1bd075df1ae39fa7
EdyValdo000 <edivaldocoelho000@example.com>
2025-01-10 05:33:28 +0100

    Exercícios 075 e 076 terminados e verificados

--- exe075.py
+++ exe075.py
@@ -1,9 +1,10 @@
-número_1º = int(input('Digite o 1º valor: '))
-número_2º = int(input('Digite o 2º valor: '))
-número_3º = int(input('Digite o 3º valor: '))
-número_4º = int(input('Digite o 4º valor: '))
 pares = 0
-números = (número_1o, número_2o, número_3o, número_4o)
+números = (int(input('Digite o 1º valor: ')), 
+    int(input('Digite o 2º valor: ')), 
+    int(input('Digite o 3º valor: ')), 
+    int(input('Digite o 4º valor: ')))
+
+print(f'Tu digitaste os números {números}')
 
 if números.count(9) == 1:
     print(f'O número 9 apareceu {números.count(9)} vez')
@@ -17,15 +18,19 @@ if números.count(3) != 0:
 else:
     print(f'O número 3 não foi digitado')
 
-
 for e in números:
     if e % 2 == 0:
-        pares += 1        
-        
+        pares += 1
 
-if pares != 0 and pares != 1:
-    print(f'Tu digitaste {pares} número par')
-elif pares >= 2:
-    print(f'Tu digitaste {pares} números pares')
+if pares == 0:
+    print('Não digitaste nenhum número par')
+elif pares == 1:
+    for e in números:
+        if e % 2 == 0:
+            print(f'O único número par que digitaste é {e}')
 else:
-    print(f'Tu não digitaste nenhum número par')
+    print(f'Os números pares que digitaste são: ',end='')
+    for e in números:        
+        if e % 2 == 0:
+            print(f'{e}',end='; ')
+print('\nFIM')
--- exe076.py
+++ exe076.py
@@ -1,10 +1,10 @@
 contador = 0
 produtos = ('Pão', 50, 'Arroz', 5000, 'Massa', 300, 'Abacate', 100)
-print('-'*30)
+print('-'*40)
 print('{:^30}'.format(' LISTA DE PRODUTOS '))
-print('-'*30)
+print('-'*40)
 while contador < len(produtos):
     print(f'{produtos[contador]:.<20}',end='')
-    print(f'{produtos[contador+1]:.>6} {'Kz':>3}')
+    print(f'{produtos[contador+1]:.>10.2f} {'Kz':>3}')
     contador += 2
-print('-'*30)
+print('-'*40)
