b255fa2fbc9167e21a7970e8b6a9cba235b74f90
EdyValdo000 <edivaldocoelho000@example.com>
2025-01-16 21:43:59 +0100

    Exercícios 88 e 89 terminados,melhorados e verificados,

--- exe088.py
+++ exe088.py
@@ -8,12 +8,16 @@ númeroDeJogos = int(input('Vais precisar de quantos jogos? '))
 
 for e in range(0, númeroDeJogos):
     while True:
-        dado = randint(0,60)
+        dado = randint(1,60)
         if dado not in sorteados:
             sorteados.append(dado)    
         if len(sorteados) == 6:
             sorteados.sort()
             jogos.append(sorteados[:])
             sorteados.clear()
-            break
-    print(f'Jogo {e+1} {jogos[e]}')
+            break    
+
+for index1, valor1 in enumerate(jogos):
+    print(f'Jogo {index1+1} ( ', end='')
+    for index2, valor2 in enumerate(jogos[index1]):
+        print(f'{jogos[index1][index2]}',end=', ' if index2 != 5 else ' )\n')
--- exe089.py
+++ exe089.py
@@ -1,7 +1,7 @@
 Alunos = list() # vai receber append ----- [aluno]
 aluno = [] # vai receber append ----- [nome, Notas]
 Notas= [] # vai receber append ----- [p1,p2]
-p1 = p2 = média = 0
+p1 = p2 = média = númeroAluno = 0
 nome = ''
 resposta = 'S'
 while True:
@@ -32,4 +32,23 @@ print(f'{'No.':<7}{'NOME':<25}{'MÉDIAS':}')
 print('--'*35)
 for index, aluno in enumerate(Alunos):
     média = (aluno[1][0] + aluno[1][1]) / 2
-    print(f'{index:<7}{aluno[0]:<25}{média:.1f}')
+    if aluno[0][-1] in 'AaUu':
+        print(f'{index:<7}\033[1;35m{aluno[0]:<25}', end='\033[33;1m' if média >= 10 else '\033[1;31m')
+        print(f'{média}\033[m')
+    else:
+        print(f'{index:<7}\033[1;32m{aluno[0]:<25}', end= '\033[33;1m' if média >= 10 else '\033[31;1m')
+        print(f'{média}\033[m')
+    
+print('')
+while True:
+    númeroAluno = int(input('Qual é a posição do aluno que queres ver as notas? [999 para parar] '))
+    if númeroAluno == 999:
+        break
+    elif númeroAluno < len(Alunos):
+        if Alunos[númeroAluno][0][-1] in 'AaUu':
+            print(f'A \033[1;35m{Alunos[númeroAluno][0]}\033[m teve P1 =','\033[1;33m' if Alunos[númeroAluno][1][0] >= 10 else '\033[31;1m',f'{Alunos[númeroAluno][1][0]}\033[m P2 =','\033[1;33m' if Alunos[númeroAluno][1][1] >= 10 else '\033[1;31m',f'{Alunos[númeroAluno][1][1]}\033[m\n')
+        else:
+            print(f'O \033[1;32m{Alunos[númeroAluno][0]}\033[m teve P1 =','\033[1;33m' if Alunos[númeroAluno][1][0] >= 10 else '\033[1;31m',f'{Alunos[númeroAluno][1][0]}\033[m P2 =','\033[1;33m' if Alunos[númeroAluno][1][1] >= 10 else '\033[1;31m',f'{Alunos[númeroAluno][1][1]}\033[m\n')
+    else:
+        print('\033[1;31mErro \033[m',end='')
+print('FIM')
