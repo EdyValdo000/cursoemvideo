79bcb4840b81c995c4d15b0f0fa438c72bf5611b
EdyValdo000 <edivaldocoelho000@example.com>
2025-01-26 14:04:05 +0100

    Validação de campos dentro de uma função

--- exe103.py
+++ exe103.py
@@ -1,12 +1,21 @@
-def ficha(nome='', golos=0):
+def ficha(nome='', Golos=''):
     resposta = ''
     if nome == '':
-        nome = '<desconhecido>'
+        nome = '<desconhecido>'    
     
+    if  Golos.isalpha or Golos == '':
+        golos = 0
+    else:
+        golos = Golos
+
     if golos != 1:
         resposta =  f'O jogador \033[33;1m{nome}\033[m marcou \033[33;1m{golos}\033[m golos no campeonato'
     else:
         resposta =  f'O jogador \033[33;1m{nome}\033[m marcou \033[33;1m{golos}\033[m golo no campeonato'    
     return resposta
 
-print(f'{ficha()}')
+
+Nome = str(input('Nome do jogador: '))
+Golos = str(input('Números de golos: ')).replace(' ','')
+print(f'{ficha(Nome,Golos)}')
+
